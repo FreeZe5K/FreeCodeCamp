5647f4d43f892bb4441363da22396d9c763963d0
Mike Hatter <mike@mikehatter.com>
2015-12-24 15:11:27 -0500

    Bonfire: Convert HTML Entities

---
+++ Intermediate Algorithm Scripting/Convert HTML Entities.js
@@ -0,0 +1,5 @@
+function convert( str ) {
+    return str.replace( /&/g, '&amp;' ).replace( /</g, '&lt;' ).replace( />/g, '&gt;' ).replace( /"/g, '&quot;' ).replace( /'/g, '&apos;' );
+}
+
+convert( 'Dolce & Gabbana' );
