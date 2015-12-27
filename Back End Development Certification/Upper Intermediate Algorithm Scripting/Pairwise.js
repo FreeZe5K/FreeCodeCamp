cc01f63a94ebcaf4ec046a642913b0e3d128e1f8
Mike Hatter <mike@mikehatter.com>
2015-12-27 15:22:44 -0500

    Bonfire: Pairwise

---
+++ Back End Development Certification/Upper Intermediate Algorithm Scripting/Pairwise.js
@@ -0,0 +1,18 @@
+function pairwise( arr, arg ) {
+    var pairs = [ ];
+
+    for( var element in arr ) {
+        var temp_element = arr[ a ];
+
+        for( var index = 1; index < arr.length; index++ ) {
+            if( temp_element + arr[ index ] === arg && index > element && pairs.indexOf( +element ) === -1 && pairs.indexOf( +index ) === -1 ) {
+                pairs.push( +element, +index );
+                break;
+            }
+        }
+    }
+
+    return ( 1 <= pairs.length ) ? pairs.reduce( function( left, right ) { return left + right; } ) : 0;
+}
+
+pairwise( [1, 4, 2, 3, 0, 5], 7 );
